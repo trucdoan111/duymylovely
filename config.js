654eaf51f0827edd8cd7d22788c7e558a33c70c2
trucdoan111 <Trannhu29122005@gmail.com>
2025-03-24 21:15:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,10 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao lại may mắn gặp ah <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'duy dang yeu nhat cua minh <33333333',
+    mess: 'dạ',
+    messDesc: 'nhắn cho e tiếp nhá',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    
